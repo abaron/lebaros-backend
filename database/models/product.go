bddb34d0b242d9fe9dcf747170f69d6640c9ee66
abaron <aryobaron@gmail.com>
2019-02-11 17:37:22 +0700

    add product model

---
+++ database/models/product.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"github.com/abaron/lebaros-backend/lib/common"
+	"github.com/jinzhu/gorm"
+)
+
+// Product data model
+type Product struct {
+	gorm.Model
+	User        User    `gorm:"foreignkey:UserID" sql:"not null"`
+	UserID      uint    `sql:"not null"`
+	Sku         string  `gorm:"size:20" sql:"not null"`
+	Name        string  `gorm:"size:128" sql:"not null"`
+	Description string  `sql:"type:text;"`
+	Price       uint    `sql:"not null"`
+	Revenue     float32 `sql:"not null"`
+	Currency    string  `gorm:"size:3" sql:"not null"`
+	Created     User    `gorm:"foreignkey:CreatedBy"`
+	CreatedBy   uint    `sql:"not null"`
+	Updated     User    `gorm:"foreignkey:UpdatedBy"`
+	UpdatedBy   uint    `gorm:"default:null"`
+	IsDeleted   bool    `gorm:"default:false" sql:"not null"`
+	Deleted     User    `gorm:"foreignkey:DeletedBy"`
+	DeletedBy   uint    `gorm:"default:null"`
+}
+
+// Serialize serializes product data
+func (p Product) Serialize() common.JSON {
+	return common.JSON{
+		"id":          p.ID,
+		"user":        p.User.Serialize(),
+		"sku":         p.Sku,
+		"name":        p.Name,
+		"description": p.Description,
+		"price":       p.Price,
+		"revenue":     p.Revenue,
+		"currency":    p.Currency,
+		"created_at":  p.CreatedAt,
+		"created_by":  p.CreatedAt,
+		"updated_at":  p.CreatedAt,
+		"updated_by":  p.CreatedAt,
+		"deleted_at":  p.CreatedAt,
+		"deleted_by":  p.CreatedAt,
+	}
+}
