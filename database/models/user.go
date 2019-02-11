a93065db77ea1264ed186990fbdc9ee90ce9893e
abaron <aryobaron@gmail.com>
2019-02-11 17:36:39 +0700

    add user model

---
+++ database/models/user.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"github.com/abaron/lebaros-backend/lib/common"
+	"github.com/jinzhu/gorm"
+)
+
+// User data model
+type User struct {
+	gorm.Model
+	Username     string
+	Fullname     string
+	PasswordHash string
+}
+
+// Serialize serializes user data
+func (u *User) Serialize() common.JSON {
+	return common.JSON{
+		"id":       u.ID,
+		"username": u.Username,
+		"fullname": u.Fullname,
+	}
+}
+
+func (u *User) Read(m common.JSON) {
+	u.ID = uint(m["id"].(float64))
+	u.Username = m["username"].(string)
+	u.Fullname = m["fullname"].(string)
+}
+
+type tabler interface {
+	TableName() string
+}
